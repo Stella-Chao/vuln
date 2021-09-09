1762bc88aa4c512205f84760c575bfa5677faf38
zhang <zhanghe0309@qq.com>
2021-09-09 15:35:28 +0800

    add detail

--- frontend/src/views/Detail.vue
+++ frontend/src/views/Detail.vue
@@ -1,25 +1,64 @@
 <template>
+  <el-card class="box-card">
+  <template #header>
+    <div class="card-header">
+      <span>漏洞详情</span>
+      <el-button class="button" type="text">check</el-button>
+    </div>
+  </template>
   <div>
-      {{ name }}
+      <span>CWE-ID: {{detail['name']}}</span>
+      <span>URL: {{detail['url']}}</span>
+      <span>CWE：{{detail['cwe_id']}}</span>
+      <span>CVSS3_Score: {{detail['cvss3_nvd_base_score']}}</span>
+      <span>CVSS3_Vector: {{detail['cvss3_nvd_vector']}}</span>
+      <span>漏洞描述: {{detail['description']}}</span>
+      <span>相关链接: {{detail['refer']}}</span>
   </div>
+</el-card>
 </template>
 
+
 <script>
-import { getDetail } from "../api/index";
+import { getLeakByCVE} from "../api/index";
 export default {
     name : "Detail",
     data () {
         return {
             name: this.$route.params, 
-            test: "hello"
+            test: "hello",
+            detail: ''
         }
     },
     mounted () {
-        getDetail(this.$route.params)
+        console.log('getDetail')
+        console.log(this.$route.params['name'])
+        getLeakByCVE(this.$route.params['name']).then((res) => {
+            console.log(res[0].fields)
+            this.detail = res[0].fields
+
+        })
     }
 }
+
 </script>
 
 <style>
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .box-card {
+    width: 480px;
+  }
 </style>
