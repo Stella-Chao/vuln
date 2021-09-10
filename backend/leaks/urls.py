c826a70b7c9a21102e41f11f06d9ff093c4bbd9b
zhang <zhanghe0309@qq.com>
2021-09-10 15:08:09 +0800

    dashboard interface

--- backend/leaks/urls.py
+++ backend/leaks/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import LeakViewSet,LeakViewSet2
+from .views import LeakViewSet,LeakViewSet2,DashBoard
 
 urlpatterns = [
     path('data/', LeakViewSet.as_view()),
-    path('leak/<str:cve>', LeakViewSet2.as_view())
+    path('leak/<str:cve>', LeakViewSet2.as_view()),
+    path('dashboard/', DashBoard.as_view())
 ]
--- backend/leaks/views.py
+++ backend/leaks/views.py
@@ -1,6 +1,7 @@
 
 # Create your views here.
 import json
+import time
 from .models import Leak
 from rest_framework.generics import ListCreateAPIView
 from .pagination import LeakPageNumberPagination
@@ -11,7 +12,7 @@ from django.core.serializers import serialize
 class LeakViewSet(ListCreateAPIView):
     queryset = Leak.objects.all()[:100]
     serializer_class = LeakSerializer
-    pagination_class = LeakPageNumberPagination
+    # pagination_class = LeakPageNumberPagination
 
     # def getDetail(self):
     #     name = self.request.GET.get('name')
@@ -25,6 +26,23 @@ class LeakViewSet2(ListCreateAPIView):
         print('get请求...')
         queryset = Leak.objects.filter(name = cve)
         json_data = serialize('json', queryset)
+        json_data = json.loads(json_data)
+        if request.method == 'GET':
+            return JsonResponse(json_data, safe=False)
+
+
+class DashBoard(ListCreateAPIView):
+    print('databoard...')
+    def get(self, request, *args, **kwargs):
+        vuln_num = Leak.objects.count()
+        '''当日新增数量'''
+        today = time.strftime("%Y-%m-%d", time.localtime())
+        today_num = Leak.objects.filter(time=today)
+        data_set = {}
+        data_set['totalNum'] = vuln_num
+        data_set['todayNum'] = today_num
+
+        json_data = serialize('json',data_set)
         json_data = json.loads(json_data)
         if request.method == 'GET':
             return JsonResponse(json_data, safe=False)
