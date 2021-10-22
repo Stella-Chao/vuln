5233031d76c7f9fc0e4c9c79fc92c122aaca6f9a
zhang <zhanghe0309@qq.com>
2021-10-22 11:33:43 +0800

    新增 "IP代理池 + 请求头"

--- script/spider/jvn_spider.py
+++ script/spider/jvn_spider.py
@@ -1,20 +1,37 @@
 import requests
 import re
+import time
+import random
 from utils.mongoUtils import connect_jvndb
 from bs4 import BeautifulSoup
+from spider_utils import user_agent_pc, get_proxy
 
 urls = []
+headers = {
+    'User-Agent': random.choice(user_agent_pc)
+}
+
 def get_urls():
+    proxy = get_proxy().get("proxy")
     base = 'https://jvndb.jvn.jp/'
     search_base = 'https://jvndb.jvn.jp/search/index.php?mode=_vulnerability_search_IA_VulnSearch&lang=ja&keyword=&useSynonym=1&vendor=&product=&datePublicFromMonth=01&datePublicFromYear={}&datePublicToMonth={}&datePublicToYear={}&datePublicToMonth={}&dateLastPublishedFromMonth=&dateLastPublishedFromYear=&dateLastPublishedToMonth=&dateLastPublishedToYear=&cwe=&searchProductId=&pageNo={}'
     # 起止日期, 每次的搜索结果不能超过10000条
-    from_year = "1998"
+    from_year = "2021"
     from_month = "01"
-    to_year = "2000"
-    to_month = "01"
+    to_year = "2021"
+    to_month = "10"
     page = 1
+    count = 0
     url = search_base.format(from_year,from_month,to_year,to_month,page)
-    res = requests.get(url)
+    # 访问被禁止后，更换代理
+    try:
+        res = requests.get(url, headers=headers, proxies={"http": "http://{}".format(proxy)}, timeout = 3)
+    except Exception as e:
+        print(e)
+        print("第{}次更换代理...".format(count))
+        count += 1
+        proxy = get_proxy().get("proxy")
+        res = requests.get(url, headers=headers, proxies={"http": "http://{}".format(proxy)},timeout=3)
     # 指定编码方式(日文),否则会出现乱码
     res.encoding = 'SHIFT_JIS'
     soup = BeautifulSoup(res.text, features="lxml")
@@ -22,6 +39,7 @@ def get_urls():
     # 获取记录总数
     str = soup.find_all('td', class_="pager_count_class")[0].get_text().strip()
     total = int(re.match('.+?(?=\件)',str).group(0))
+    print('total:',total)
     # 获取总页数
     if total % 100 == 0:
         pages = total / 100
@@ -31,7 +49,15 @@ def get_urls():
 
     while page <= pages:
         url = search_base.format(from_year, from_month, to_year, to_month, page)
-        res = requests.get(url)
+        print(url)
+        try:
+            res = requests.get(url, headers=headers, proxies={"http": "http://{}".format(proxy)}, timeout=3)
+        except Exception as e:
+            print(e)
+            print("第{}次更换代理...".format(count))
+            count += 1
+            proxy = get_proxy().get("proxy")
+            res = requests.get(url, headers=headers, proxies={"http": "http://{}".format(proxy)}, timeout=3)
         # 指定编码方式(日文),否则会出现乱码
         res.encoding = 'SHIFT_JIS'
         soup = BeautifulSoup(res.text, features="lxml")
@@ -54,7 +80,14 @@ def parse(urls):
         print('正在抓取第{}个页面...'.format(count))
         jvn = {}
         jvn['url'] = url
-        res = requests.get(url)
+        try:
+            res = requests.get(url, headers=headers, proxies={"http": "http://{}".format(proxy)}, timeout=3)
+        except Exception as e:
+            print(e)
+            print("第{}次更换代理...".format(count))
+            count += 1
+            proxy = get_proxy().get("proxy")
+            res = requests.get(url, headers=headers, proxies={"http": "http://{}".format(proxy)},timeout=3)
         # 指定编码方式(日文),否则会出现乱码
         res.encoding = 'SHIFT_JIS'
         soup = BeautifulSoup(res.text, features="lxml")
@@ -62,13 +95,15 @@ def parse(urls):
         rows = table.find_all('tr')
         '''注意：不同页面按行取值的方式可不可行？'''
         jvn['jvn_id'] = rows[0].text.replace('\n','')
-        jvn['title'] = rows[1].text.replace('\n','')
+        jvn['title'] = rows[1].text.replace('\n','').replace('\xa0',' ')
         jvn['abstract'] = rows[3].text.replace('\n','')
         cvss = rows[5].find_all('div',class_='float_left')
-        jvn['cvss2'] = cvss[0].text
-        # 判断是否存在cvss3
+        # 判断是否存在 cvss2
+        if len(cvss) > 0:
+            jvn['cvss2'] = cvss[0].text
+        # 判断是否存在 cvss3
         if len(cvss) > 1:
-            jvn['cvss3'] = rows[5].find_all('div',class_='float_left')[1].text
+            jvn['cvss3'] = cvss[1].text
         # 受影响的系统
         jvn['affected'] = rows[8].text.replace('\n','')
         # 预期影响
@@ -97,6 +132,7 @@ def parse(urls):
         # 插入vuln.jvndb集合中
         coll = connect_jvndb()
         coll.insert_one(jvn)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
