22ffec8e903f281d51f47468bec7a9e0ae546276
zhang <zhanghe0309@qq.com>
2021-10-29 16:54:18 +0800

    新增 "前端模块"

---
+++ frontend2/src/views/Detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      {{ vuln }}
+  </div>
+</template>
+
+<script>
+export default {
+    name = "Detail",
+    data () {
+        return {
+            vuln: this.$router.params
+        }
+    },
+    mounted () {
+        let params = {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
