6f491a62e18a28a71635c35bc6ed604696434ce7
zhang <zhanghe0309@qq.com>
2022-05-18 15:42:01 +0800

    删除 “旧版本代码”

--- backend/Test/test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def get_time():
-    # 格式化成2021-09-10 11:45:39形式
-    cur_time = time.strftime("%Y-%m-%d", time.localtime())
-    print(cur_time)
-
-if __name__ == '__main__':
-    get_time()
--- backend/leaks/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Leak
-
-class LeakSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Leak
-        fields = '__all__'
