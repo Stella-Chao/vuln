6d5f7157c4f37f993073882c8998a42924723f79
zhang <zhanghe0309@qq.com>
2022-08-09 17:41:33 +0800

    更改翻译方式

--- script/spider/translate.py
+++ script/spider/translate.py
@@ -5,6 +5,13 @@ import random
 import json
 from hashlib import md5
 
+import json
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.tmt.v20180321 import tmt_client, models
+
 # 每月可免费翻译 5w字符 (baidu API)
 appid = '20200202000379572'
 appkey = 'pjNWyL2apr4DOb1qeaPh'
@@ -20,7 +27,7 @@ def make_md5(s, encoding='utf-8'):
     return md5(s.encode(encoding)).hexdigest()
 
 # 翻译模块 参数 (文本，源语言，目标语言)
-def translate(query, from_lang, to_lang):
+def baidu_translate(query, from_lang, to_lang):
     salt = random.randint(32768, 65536)
     sign = make_md5(appid + query + str(salt) + appkey)
 
@@ -34,3 +41,38 @@ def translate(query, from_lang, to_lang):
     # Show response
     print(json.dumps(result, indent=4, ensure_ascii=False))
     return result['trans_result'][0]['dst']
+
+
+
+def translate(query, from_lang, to_lang):
+    try:
+        # 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey,此处还需注意密钥对的保密
+        # 密钥可前往https://console.cloud.tencent.com/cam/capi网站进行获取
+        cred = credential.Credential("SecretId", "SecretKey")
+        # 实例化一个http选项，可选的，没有特殊需求可以跳过
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "tmt.tencentcloudapi.com"
+
+        # 实例化一个client选项，可选的，没有特殊需求可以跳过
+        clientProfile = ClientProfile()
+        clientProfile.httpProfile = httpProfile
+        # 实例化要请求产品的client对象,clientProfile是可选的
+        client = tmt_client.TmtClient(cred, "ap-beijing", clientProfile)
+
+        # 实例化一个请求对象,每个接口都会对应一个request对象
+        req = models.TextTranslateRequest()
+        params = {
+            "SourceText": query,
+            "Source": from_lang,
+            "Target": to_lang,
+            "ProjectId": 1
+        }
+        req.from_json_string(json.dumps(params))
+
+        # 返回的resp是一个TextTranslateResponse的实例，与请求对象对应
+        resp = client.TextTranslate(req)
+        # 输出json格式的字符串回包
+        print(resp.to_json_string())
+
+    except TencentCloudSDKException as err:
+        print(err)
