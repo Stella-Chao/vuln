32aca8cc6b1cdf9c3246dbb9494d446a2e3f06a5
zhang <zhanghe0309@qq.com>
2022-08-16 17:34:29 +0800

    test

--- spiders/jvn_spider/jvn_spider/spiders/jvn.py
+++ spiders/jvn_spider/jvn_spider/spiders/jvn.py
@@ -19,47 +19,45 @@ class JVNSpider(scrapy_redis.spiders.RedisSpider):
     def start_requests(self):
         base = 'https://jvndb.jvn.jp'
         search_base = 'https://jvndb.jvn.jp/search/index.php?mode=_vulnerability_search_IA_VulnSearch&lang=ja&keyword=&useSynonym=1&vendor=&product=&datePublicFromYear={}&datePublicFromMonth={}&datePublicToYear={}&datePublicToMonth={}&dateLastPublishedFromMonth=&dateLastPublishedFromYear=&dateLastPublishedToMonth=&dateLastPublishedToYear=&cwe=&searchProductId=&pageNo={}'
-        from_year = "2010"
-        from_month = "06"
-        to_year = "2010"
-        to_month = "06"
-        page = 1
+        year = "2009"
         urls = []
-        url = search_base.format(from_year,from_month,to_year,to_month,page)
-        res = requests.get(url, verify=False)
-        res.encoding = 'SHIFT_JIS'
-        soup = BeautifulSoup(res.content, "html.parser")
-        print(soup.title.string)
-        all_text = soup.select(".pager_class .pager_count_class")[0].text
-        total = re.findall(r'^\D*(\d+)', all_text)
-        total = (int)(total[0])
-        print('total:', total)
-        # 获取总页数
-        if total % 100 == 0:
-            pages = total / 100
-        else:
-            pages = total / 100 + 1
-        print(total)
-        while page <= pages:
-            # proxy = self.get_proxy()
-            url = search_base.format(from_year, from_month, to_year, to_month, page)
-            print(url)
-            print("--------------------正在爬取第{}页-------------------".format(page))
-            time.sleep(5)
-            # res = requests.get(url,verify = False, proxies={"http": "http://{}".format(proxy)})
+        months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+        for month in months:
+            page = 1
+            url = search_base.format(year,month,year,month,page)
             res = requests.get(url, verify=False)
-            # 指定编码方式(日文),否则会出现乱码
             res.encoding = 'SHIFT_JIS'
             soup = BeautifulSoup(res.content, "html.parser")
-            tr_list = soup.select('table.result_class tr')
-            for tr in tr_list:
-                td = tr.find_all('td')
-                if len(td) > 0:
-                    url = base + td[0].a['href']
-                    print(url)
-                    urls.append(url)
-            page += 1
-        print(urls)
+            print(soup.title.string)
+            all_text = soup.select(".pager_class .pager_count_class")[0].text
+            total = re.findall(r'^\D*(\d+)', all_text)
+            total = (int)(total[0])
+            print('total:', total)
+            # 获取总页数
+            if total % 100 == 0:
+                pages = total / 100
+            else:
+                pages = total / 100 + 1
+            print(total)
+            while page <= pages:
+                # proxy = self.get_proxy()
+                url = search_base.format(year, month, year, month, page)
+                print(url)
+                print("--------------------正在爬取第{}页-------------------".format(page))
+                time.sleep(5)
+                # res = requests.get(url,verify = False, proxies={"http": "http://{}".format(proxy)})
+                res = requests.get(url, verify=False)
+                # 指定编码方式(日文),否则会出现乱码
+                res.encoding = 'SHIFT_JIS'
+                soup = BeautifulSoup(res.content, "html.parser")
+                tr_list = soup.select('table.result_class tr')
+                for tr in tr_list:
+                    td = tr.find_all('td')
+                    if len(td) > 0:
+                        url = base + td[0].a['href']
+                        print(url)
+                        urls.append(url)
+                page += 1
         for link in urls:
             yield scrapy.Request(url=link, encoding='SHIFT_JIS', callback=self.parse2, dont_filter=True)
 
